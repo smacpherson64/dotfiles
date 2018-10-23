4ade9b3bac97b54ff1620d44f8029e3491143001
Seth MacPherson <smacpherson64@gmail.com>
2018-10-23 17:02:39 -0500

    simplified grc

--- system/grc.sh
+++ system/grc.sh
@@ -1,5 +1,2 @@
 # GRC colorizes nifty unix tools all over the place
-if [ type 'grc' ] && [ type 'brew' ];
-then
-  source `brew --prefix`/etc/grc.bashrc
-fi
+type 'grc' > /dev/null && type 'brew' > /dev/null &&  source `brew --prefix`/etc/grc.bashrc
