4a53a1544ecd51aadc29f570fc51e6e2abd18e84
Seth MacPherson <smacpherson64@gmail.com>
2021-09-26 17:04:02 -0500

    adjusts prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -10,53 +10,41 @@ else
 fi
 
 git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
+  ref=$($git symbolic-ref HEAD 2>/dev/null) || return
+  echo "on %{$fg_bold[magenta]%}${ref#refs/heads/}%{$reset_color%}"
 }
 
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo ""
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    fi
-  fi
-}
-
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "${ref#refs/heads/}"
-}
-
-# This assumes that you always have an origin named `origin`, and that you only
-# care about one specific origin. If this is not the case, you might want to use
-# `$git cherry -v @{upstream}` instead.
-need_push () {
+git_status () {
   if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
   then
     number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
+    branch=$($git symbolic-ref HEAD 2>/dev/null)
 
-    if [[ $number == 0 ]]
+    if [[ $number ]]
     then
-      echo " "
-    else
-      echo " with %{$fg_bold[magenta]%}$number unpushed%{$reset_color%}"
+      echo "%{$fg_bold[white]%} (↑$number)%{$reset_color%}"
+    else 
+      echo "%{$fg_bold[magenta]%} (no remote)%{$reset_color%}"
     fi
   fi
 }
 
-directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+user_name() {
+  echo "%{$fg_bold[cyan]%}$(whoami)%{$reset_color%}"
 }
 
-export PROMPT=$'\nin $(directory_name) $(git_dirty)$(need_push)\n› '
+directory_name() {
+  echo "%{$fg_bold[green]%}%2/%\/%{$reset_color%}"
+}
+
+branch_name() {
+  echo "%{$fg_bold[magenta]%}git_branch%{$reset_color%}"
+}
+
+export PROMPT=$'\n$(user_name) at $(directory_name) $(git_branch)$(git_status)\n› '
+
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+  export RPROMPT="%{$fg_bold[orange]%}%{$reset_color%}"
 }
 
 precmd() {
