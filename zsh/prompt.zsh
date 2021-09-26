9397a419255a79d62be6f0f7a211a868ff40799a
Seth MacPherson <smacpherson64@gmail.com>
2021-09-26 17:07:02 -0500

    simplifies prompt more

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -14,18 +14,6 @@ git_branch() {
   echo "on %{$fg_bold[magenta]%}${ref#refs/heads/}%{$reset_color%}"
 }
 
-git_status () {
-  if [ $($git rev-parse --is-inside-work-tree 2>/dev/null) ]
-  then
-    number=$($git cherry -v origin/$(git symbolic-ref --short HEAD) 2>/dev/null | wc -l | bc)
-    branch=$($git symbolic-ref HEAD 2>/dev/null)
-
-    if [[ $number > 0 ]]
-    then
-      echo "%{$fg_bold[white]%} (↑$number)%{$reset_color%}"
-    fi
-  fi
-}
 
 user_name() {
   echo "%{$fg_bold[cyan]%}$(whoami)%{$reset_color%}"
@@ -39,7 +27,7 @@ branch_name() {
   echo "%{$fg_bold[magenta]%}git_branch%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(user_name) at $(directory_name) $(git_branch)$(git_status)\n› '
+export PROMPT=$'\n$(user_name) at $(directory_name) $(git_branch)\n› '
 
 set_prompt () {
   export RPROMPT="%{$fg_bold[orange]%}%{$reset_color%}"
